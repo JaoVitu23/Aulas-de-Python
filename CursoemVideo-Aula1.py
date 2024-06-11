59e2676b66113e69d70a9ec94eadc7cf357576fc
JaoVitu23 <160781356+JaoVitu23@users.noreply.github.com>
2024-06-11 10:52:27 -0300

    Add files via upload
    Essa aula foi focada em aprender a como utilizar variáveis e como guardá-las para printar para o usuário posteriormente, ainda utilizamos um sistema de soma.

---
+++ CursoemVideo-Aula1.py
@@ -0,0 +1,15 @@
+#Essa aula foi focada em aprender a como utilizar variavéis e como guardá-las 
+# para printar para o usuário posteriomente, ainda utilizamos um sistema de soma
+
+nome = input('Qual o seu nome? ')
+print('Boas-Vindas', nome,',Prazer!')
+
+dia = input('Digite o dia de seu nascimento: ')
+mes = input('Digite o mês, por extenso, de seu nascimento: ')
+ano = input('Digite o ano de seu nascimento: ')
+print('Você nasceu no dia',dia,'de',mes,'no ano de',ano)
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+resultado = num1 + num2
+print('O resultado da soma desses dois números é de:',resultado)
